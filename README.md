17d655e260412ae26f338bb76664f0accbf0c63b
Ondratra <ondratra@ondratra.eu>
2017-02-24 01:31:24 +0100

    nested sync/async function calls test

--- README.md
+++ README.md
@@ -12,8 +12,12 @@ $ npm run test
 > es-performance-tests@0.1.0 test /mnt/work/es-performance-tests
 > node --harmony-async-await index.js
 
-Testing difference between sync and async function calls: (100000 iterations)
-Async: 790ms
-Sync: 53ms
-Async is ~14 times slower
+Testing difference between sync and async function calls: (100 000 iterations)
+Async: 803ms
+Sync: 52ms
+Async is ~15 times slower
+Testing difference between nested sync and async function calls: (100 000 iterations)
+Async: 1288ms
+Sync: 58ms
+Async is ~22 times slower
 ```
--- index.js
+++ index.js
@@ -9,22 +9,39 @@ async function sync1() {
     return counter2++;
 }
 
+let counter3 = 0;
+async function async2() { // nested
+    const innerFunc = async function (tmp) {
+        return tmp + tmp % 2;
+    };
+
+    return await innerFunc(++counter3);
+}
+
+let counter4 = 0;
+async function sync2() {
+    const innerFunc = function (tmp) {
+        return tmp + tmp % 2;
+    };
+
+    return innerFunc(++counter4);
+}
 
 
-async function test1(iterationCount) {
+async function testAsync(iterationCount, testFunc) {
     const start = (new Date).getTime();
     for (let i = 0; i < iterationCount; i++) {
-        await async1();
+        await testFunc();
     }
     const end = (new Date).getTime();
 
     return end - start;
 }
 
-async function test2(iterationCount) {
+async function testSync(iterationCount, testFunc) {
     const start = (new Date).getTime();
     for (let i = 0; i < iterationCount; i++) {
-        sync1();
+        testFunc();
     }
     const end = (new Date).getTime();
 
@@ -32,16 +49,38 @@ async function test2(iterationCount) {
 }
 
 
-async function run() {
+
+function formatThousands(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+async function testeSet1() {
     let iterationCount = 100000;
 
-    const result1 = await test1(iterationCount);
-    const result2 = await test2(iterationCount);
+    const result1 = await testAsync(iterationCount, async1);
+    const result2 = await testSync(iterationCount, sync1);
 
-    console.log('Testing difference between sync and async function calls: (' + iterationCount + ' iterations)')
+    console.log('Testing difference between sync and async function calls: (' + formatThousands(iterationCount) + ' iterations)')
     console.log('Async: ' + result1 + 'ms');
     console.log('Sync: ' + result2 + 'ms');
     console.log('Async is ~' + parseInt(result1 / result2) + ' times slower');
 }
 
-run ();
+async function testeSet2() {
+    let iterationCount = 100000;
+
+    const result1 = await testAsync(iterationCount, async2);
+    const result2 = await testSync(iterationCount, sync2);
+
+    console.log('Testing difference between nested sync and async function calls: (' + formatThousands(iterationCount) + ' iterations)')
+    console.log('Async: ' + result1 + 'ms');
+    console.log('Sync: ' + result2 + 'ms');
+    console.log('Async is ~' + parseInt(result1 / result2) + ' times slower');
+}
+
+async function runAllTest() {
+    await testeSet1();
+    await testeSet2();
+}
+
+runAllTest();
